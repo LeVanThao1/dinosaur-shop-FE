92899aa66167423016028ec066e955409cec7b86
Your Name <you@example.com>
2020-12-16 08:50:08 +0700

    doing

--- src/app/store.js
+++ src/app/store.js
@@ -2,15 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slice/users.slice";
 import authReducer from "../slice/auth.slice";
 import tokenReducer from "../slice/token.slice";
+import productReducer from "../slice/products.slice";
+import socketReducer from "../slice/socket.slice";
 
 const rootReducer = {
-  users: usersReducer,
-  auth: authReducer,
-  token: tokenReducer,
+	users: usersReducer,
+	auth: authReducer,
+	token: tokenReducer,
+	products: productReducer,
+	socket: socketReducer,
 };
 
 const store = configureStore({
-  reducer: rootReducer,
+	reducer: rootReducer,
 });
 
 export default store;
